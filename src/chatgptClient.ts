de6b960d2a06f9b781266c6565b095671c6cfd3d
Jacopo <jacopo.carlini@gmail.com>
2023-04-15 18:21:10 +0200

    done

--- src/chatgptClient.ts
+++ src/chatgptClient.ts
@@ -5,45 +5,42 @@ import {promptForJson} from './prompt'
 import {Suggestions} from './utils'
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(configuration)
 
 export async function getSuggestions(
-    textWithLineNumber: string,
-    linesToReview: { start: number; end: number }[]
+  textWithLineNumber: string,
+  linesToReview: {start: number; end: number}[]
 ): Promise<Suggestions> {
-    let prompt = promptForJson(
-        textWithLineNumber,
-        linesToReview.map(({start, end}) => `line ${start}-${end}`).join(',')
-    );
+  const prompt = promptForJson(
+    textWithLineNumber,
+    linesToReview.map(({start, end}) => `line ${start}-${end}`).join(',')
+  )
 
-    console.log(prompt);
-    const response = await openai.createCompletion({
-        model: 'text-davinci-003',
-        max_tokens: 2048,
-        prompt: prompt
-    })
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    max_tokens: 2048,
+    prompt
+  })
 
-    // extract the json from the response
-    const result = response.data.choices[0].text ?? ''
+  // extract the json from the response
+  const result = response.data.choices[0].text ?? ''
 
-    console.log(result)
-    // console.log(JSON.parse(response.data.choices[0].text ?? ''))
-    const startIndex = result.indexOf('{')
-    const endIndex = result.lastIndexOf('}')
-    const json =
-        startIndex !== -1 && endIndex !== -1 && endIndex > startIndex
-            ? result.substring(startIndex, endIndex + 1)
-            : ''
+  const startIndex = result.indexOf('{')
+  const endIndex = result.lastIndexOf('}')
+  const json =
+    startIndex !== -1 && endIndex !== -1 && endIndex > startIndex
+      ? result.substring(startIndex, endIndex + 1)
+      : ''
 
-    let suggestions
-    try {
-        suggestions = JSON.parse(json)
-    } catch (err) {
-        throw new Error(
-            `ChatGPT response is not a valid json:\n ${response.data.choices[0].text}`
-        )
-    }
-    return suggestions
+  let suggestions
+  try {
+    suggestions = JSON.parse(json)
+  } catch (err) {
+    throw new Error(
+      `ChatGPT response is not a valid json:\n ${response.data.choices[0].text}`
+    )
+  }
+  return suggestions
 }
--- src/main.ts
+++ src/main.ts
@@ -6,73 +6,73 @@ import {getRawFileContent, postCommentToPR, processSuggestions} from './client'
 import {getSuggestions} from './chatgptClient'
 import {promptForText} from './prompt'
 import {
-    addLineNumbers,
-    getChangedLineNumbers,
-    validateSuggestions
+  addLineNumbers,
+  getChangedLineNumbers,
+  validateSuggestions
 } from './utils'
 
 const octokit = new Octokit()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(configuration)
 
 async function run(): Promise<void> {
-    try {
-        const extensions = core.getInput('extensions').split(',')
+  try {
+    const extensions = core.getInput('extensions').split(',')
 
-        const pullNumber = parseInt(core.getInput('number'))
-        const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/')
+    const pullNumber = parseInt(core.getInput('number'))
+    const [owner, repo] = process.env.GITHUB_REPOSITORY!.split('/')
 
-        const files = await octokit.request(
-            `GET /repos/${owner}/${repo}/pulls/${pullNumber}/files`
-        )
+    const files = await octokit.request(
+      `GET /repos/${owner}/${repo}/pulls/${pullNumber}/files`
+    )
 
-        for (const file of files.data) {
-            const extension = file.filename.split('.').pop()
+    for (const file of files.data) {
+      const extension = file.filename.split('.').pop()
 
-            if (extensions.includes(extension)) {
-                const text = await getRawFileContent(file.raw_url)
-                const textWithLineNumber = addLineNumbers(text!)
-                if (process.env.CODEGUARD_COMMENT_BY_LINE) {
-                    const changedLines = getChangedLineNumbers(file.patch)
-                    const suggestions = await getSuggestions(
-                        textWithLineNumber,
-                        changedLines
-                    )
+      if (extensions.includes(extension)) {
+        const text = await getRawFileContent(file.raw_url)
+        const textWithLineNumber = addLineNumbers(text!)
+        if (process.env.CODEGUARD_COMMENT_BY_LINE) {
+          const changedLines = getChangedLineNumbers(file.patch)
+          const suggestions = await getSuggestions(
+            textWithLineNumber,
+            changedLines
+          )
 
-                    validateSuggestions(suggestions)
+          validateSuggestions(suggestions)
 
-                    await processSuggestions(
-                        file,
-                        suggestions,
-                        owner,
-                        repo,
-                        pullNumber,
-                        octokit,
-                        changedLines
-                    )
-                } else {
-                    const response = await openai.createCompletion({
-                        model: 'text-davinci-003',
-                        max_tokens: 2048,
-                        prompt: promptForText(file.filename, textWithLineNumber)
-                    })
+          await processSuggestions(
+            file,
+            suggestions,
+            owner,
+            repo,
+            pullNumber,
+            octokit,
+            changedLines
+          )
+        } else {
+          const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            max_tokens: 2048,
+            prompt: promptForText(file.filename, textWithLineNumber)
+          })
 
-                    await postCommentToPR(
-                        owner,
-                        repo,
-                        pullNumber,
-                        response.data.choices[0].text ?? '',
-                        octokit
-                    )
-                }
-            }
+          await postCommentToPR(
+            owner,
+            repo,
+            pullNumber,
+            response.data.choices[0].text ?? '',
+            octokit
+          )
         }
-    } catch (error) {
-        if (error instanceof Error) core.debug(error.message)
+      }
     }
+  } catch (error) {
+    if (error instanceof Error) core.debug(error.message)
+  }
 }
 
 run()
